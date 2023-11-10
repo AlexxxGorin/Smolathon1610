c6ba9d9ef9f1f0fcd74dab03b47da652cc79706e
Tanjiro00 <67290783+Tanjiro00@users.noreply.github.com>
2023-11-10 21:58:06 +0300

    Update User_utils.py

--- User_utils.py
+++ User_utils.py
@@ -24,12 +24,6 @@ def _mean_pooling(model_output, attention_mask):
     sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
     return sum_embeddings / sum_mask
 
-def _mean_pooling(model_output, attention_mask):
-    token_embeddings = model_output[0] #First element of model_output contains all token embeddings
-    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-    sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
-    sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
-    return sum_embeddings / sum_mask
 
 def encode_person_text(input_string, text_tokenizer, vec_model):
     '''
